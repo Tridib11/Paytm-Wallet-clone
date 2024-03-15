1aeeb6e74185683f6e7e57031322441847237e3f
Tridib11 <tridibghosh12345@gmail.com>
2024-03-16 00:29:14 +0530

    File added

---
+++ backend/routes/user.js
@@ -0,0 +1,154 @@
+const express = require("express");
+const zod = require("zod");
+const bcrypt = require("bcrypt");
+const { User, Account } = require("../db");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = require("../config");
+const { authMiddleware } = require("../middleware");
+
+const signupBody = zod.object({
+  username: zod.string().email(),
+  password: zod.string(),
+  firstName: zod.string(),
+  lastName: zod.string(),
+});
+
+router.post("/signup", async (req, res) => {
+  const { success } = signupBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Email already taken/Incorrect inputs",
+    });
+  }
+  const existingUser = await User.findOne({
+    username: req.body.username,
+  });
+  if (existingUser) {
+    return res.status(411).json({
+      message: "Email already taken/Incorrect Inputs",
+    });
+  }
+
+//   const saltRounds = 10;
+//   const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+  const user = await User.create({
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  });
+
+  const userId = user._id;
+
+  //Adding some money to the account
+  await Account.create({
+    userId,
+    balance:1+Math.random()*10000
+  })
+
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+
+  res.json({
+    message: "User created Successfully",
+    token: token,
+  });
+});
+
+
+//Signin
+
+const signinBody = zod.object({
+  username: zod.string().email(),
+password: zod.string()
+})
+
+router.post("/signin", async (req, res) => {
+  const { success } = signinBody.safeParse(req.body)
+  if (!success) {
+      return res.status(411).json({
+          message: "Incorrect inputs"
+      })
+  }
+
+  const user = await User.findOne({
+      username: req.body.username,
+      password: req.body.password
+  });
+
+  if (user) {
+      const token = jwt.sign({
+          userId: user._id
+      }, JWT_SECRET);
+
+      res.json({
+          token: token
+      })
+      return;
+  }
+
+  
+  res.status(411).json({
+      message: "Error while logging in"
+  })
+})
+
+
+
+///   Update USER - Route to update user information
+
+const updateBody=zod.object({
+    password:zod.string().optional(),
+    firstName:zod.string().optional(),
+    lastName:zod.string().optional()
+})
+
+
+router.put("/",authMiddleware,async(req,res)=>{
+    const {success} = updateBody.safeParse(req.body)
+    if(!success){
+        res.status(411).json({
+            message:"Error while updating information"
+        })
+    }
+    await User.updateOne({_id:req.userId},req.body)
+    res.json({
+        message:"Updated Successfully"
+    })
+})
+
+
+
+// Route to get users from the backend, filterable via firstName/lastName
+
+router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
+module.exports = router;
