9b90c706648050368a698ba4719f6616d79c50bb
Tridib11 <tridibghosh12345@gmail.com>
2024-03-17 04:27:05 +0530

    File added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-
-import './App.css'
+import {Routes,BrowserRouter, Route} from "react-router-dom"
+import {Signup} from "./pages/Signup"
+import {Signin} from "./pages/Signin"
+import {Dashboard} from "./pages/Dashboard"
+import {SendMoney} from "./pages/Dashboard"
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/singup" element={<Signup />}/>
+        <Route path="/singin" element={<Signin />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/send" element={<SendMoney />}/>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
