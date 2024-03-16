d73f7e2ec4fdcce54bc2660fff59e09b201e11df
Tridib11 <tridibghosh12345@gmail.com>
2024-03-17 04:49:08 +0530

    File added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,22 @@
-import {Routes,BrowserRouter, Route} from "react-router-dom"
-import {Signup} from "./pages/Signup"
-import {Signin} from "./pages/Signin"
-import {Dashboard} from "./pages/Dashboard"
-import {SendMoney} from "./pages/Dashboard"
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+// import { Dashboard } from "./pages/Dashboard";
+// import { SendMoney } from "./pages/SendMoney";
 
 function App() {
-
-  
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path="/singup" element={<Signup />}/>
-        <Route path="/singin" element={<Signin />}/>
-        <Route path="/dashboard" element={<Dashboard />}/>
-        <Route path="/send" element={<SendMoney />}/>
-      </Routes>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} /> */}
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
