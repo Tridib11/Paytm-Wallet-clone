39a43625c69822926203fdfc5bb41cdd3a46ca1b
Tridib11 <tridibghosh12345@gmail.com>
2024-03-18 00:50:48 +0530

    File added

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,36 @@
 import { Appbar } from "../components/Appbar"
 import { Balance } from "../components/Balance"
 import { Users } from "../components/Users"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 export const Dashboard = () => {
-    return <div>
-        <Appbar />
-        <div className="m-8">
-            <Balance value={"10,000"} />
-            <Users />
+    const [balance, setBalance] = useState(0);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("http://localhost:3000/api/v1/account/balance", {
+                    headers: {
+                        Authorization: "Bearer " + localStorage.getItem("token")
+                    }
+                });
+                setBalance(response.data.balance);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []); // Empty dependency array ensures this effect runs only once
+
+    return (
+        <div>
+            <Appbar />
+            <div className="m-8">
+                <Balance value={balance} />
+                <Users />
+            </div>
         </div>
-    </div>
+    );
 }
